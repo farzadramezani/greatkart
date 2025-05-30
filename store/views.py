308ae73e1e0fb80524522978852a2a80b8c45ef5
farzadramezani <farzad.ramezani@gmail.com>
2025-05-31 01:37:20 +0330

    store and carts functionality

--- store/views.py
+++ store/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product, ReviewRating, ProductGallery
 from category.models import Category
-#from carts.models import CartItem
+from cart.models import CartItem
 from django.db.models import Q
 
-#from carts.views import _cart_id
+from cart.views import _cart_id
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
-from django.http import HttpResponse
 #from .forms import ReviewForm
 from django.contrib import messages
 #from orders.models import OrderProduct
@@ -19,13 +18,13 @@ def store(request, category_slug=None):
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
-        paginator = Paginator(products, 6)
+        paginator = Paginator(products, 2)
         page = request.GET.get('page')
         paged_products = paginator.get_page(page)
         product_count = products.count()
     else:
         products = Product.objects.all().filter(is_available=True).order_by('id')
-        paginator = Paginator(products, 6)
+        paginator = Paginator(products, 2)
         page = request.GET.get('page')
         paged_products = paginator.get_page(page)
         product_count = products.count()
@@ -40,7 +39,8 @@ def store(request, category_slug=None):
 def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
-        #in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
+
     except Exception as e:
         raise e
 
@@ -60,7 +60,7 @@ def product_detail(request, category_slug, product_slug):
 
     context = {
         'single_product': single_product,
-        #'in_cart'       : in_cart,
+        'in_cart'       : in_cart,
         #'orderproduct': orderproduct,
         'reviews': reviews,
         'product_gallery': product_gallery,
@@ -68,7 +68,7 @@ def product_detail(request, category_slug, product_slug):
     return render(request, 'store/product_detail.html', context)
 
 
-'''def search(request):
+def search(request):
     if 'keyword' in request.GET:
         keyword = request.GET['keyword']
         if keyword:
@@ -81,7 +81,7 @@ def product_detail(request, category_slug, product_slug):
     return render(request, 'store/store.html', context)
 
 
-def submit_review(request, product_id):
+'''def submit_review(request, product_id):
     url = request.META.get('HTTP_REFERER')
     if request.method == 'POST':
         try:
